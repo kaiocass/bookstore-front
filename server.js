3d1d53a7f2fb4279ad551075b187664a4cb79fc2
Kaio Cassiano <imkaiocass@gmail.com>
2021-08-06 21:12:32 -0300

    configurando para deploy

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Importa as dependências que acabamos de instalar
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/bookstore-front"));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/bookstore-front/index.html"));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
